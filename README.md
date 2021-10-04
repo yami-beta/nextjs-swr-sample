5c65fb630932f9cc5d9663eebd00adeac9f3e38b
yami-beta <yami-beta@users.noreply.github.com>
2021-10-04 13:01:53 +0900

    chore: initialize Next.js app

---
+++ README.md
@@ -0,0 +1,1 @@
+# nextjs-swr-sample
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+type IndexPageProps = {};
+
+const IndexPage: NextPage<IndexPageProps> = () => {
+  return (
+    <div>
+      <h1>Index</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
